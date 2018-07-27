4796728bb612fb06f8d97c5840160883c1ecb1f1
venkatshankar <35727864+venkatshankar@users.noreply.github.com>
2018-07-27 14:24:53 +0530

    Create areaoftriangle.c

---
+++ areaoftriangle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int b,h;
+    float a;
+    printf("enter the breath and height:");
+    scanf("%d%d",&b,&h);
+    a=((1/2)*b*h);
+    printf("Area:%f",a);
+}
